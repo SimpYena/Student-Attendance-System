75b8dd04a9ba432d93aece52148ff6f22f745b7d
SimpYena <jointhe611@gmail.com>
2023-11-20 22:13:19 +0700

    scale button and images flexible with any windows

--- main.py
+++ main.py
@@ -14,79 +14,79 @@ class Student_Attendance_Sys:
 
         #first image
         img = Image.open(r"assets/collectedImages/vku.jpg")
-        img=img.resize((500,130), Image.LANCZOS)
+        img=img.resize((int(screen_width/3),130), Image.LANCZOS)
         self.photoimg=ImageTk.PhotoImage(img)
 
         f_lbl=Label(self.root,image=self.photoimg)
-        f_lbl.place(x=0,y=0,width=500,height=130)
+        f_lbl.place(x=0,y=0,width=screen_width/3,height=130)
 
 
         #second image
         img1 = Image.open(r"assets/collectedImages/vku.jpg")
-        img1=img1.resize((500,130), Image.LANCZOS)
+        img1=img1.resize((int(screen_width/3),130), Image.LANCZOS)
         self.photoimg1=ImageTk.PhotoImage(img1)
 
         f_lbl=Label(self.root,image=self.photoimg1)
-        f_lbl.place(x=500,y=0,width=500,height=130)
+        f_lbl.place(x=screen_width/3,y=0,width=screen_width/3,height=130)
 
 
 
         #third image
         img2 = Image.open(r"assets/collectedImages/vku.jpg")
-        img2=img2.resize((500,130), Image.LANCZOS)
+        img2=img2.resize((int(screen_width/3),130), Image.LANCZOS)
         self.photoimg2=ImageTk.PhotoImage(img2)
 
         f_lbl=Label(self.root,image=self.photoimg2)
-        f_lbl.place(x=1000,y=0,width=550,height=130)
+        f_lbl.place(x=2*screen_width/3,y=0,width=screen_width/3,height=130)
 
         #background image
         img3 = Image.open(r"assets/collectedImages/background.jpg")
-        img3=img3.resize((1530,710), Image.LANCZOS)
+        img3=img3.resize((screen_width,screen_height), Image.LANCZOS)
         self.photoimg3=ImageTk.PhotoImage(img3)
 
         bg_img=Label(self.root,image=self.photoimg3)
-        bg_img.place(x=0,y=130,width=1530,height=710)
+        bg_img.place(x=0,y=130,width=screen_width,height=screen_height)
 
 
         title_lbl=Label(bg_img, text="FACE RECOGNITION ATTENDANCE SYSTEM SOFTWARE", font=("times new roman",35,"bold"),bg="white",fg="red")
-        title_lbl.place(x=0,y=0,width=1530,height=45)
+        title_lbl.place(x=0,y=0,width=screen_width,height=45)
 
 
         #student button
         img4 = Image.open(r"assets/collectedImages/student_detail.jpg")
-        img4=img4.resize((220,220), Image.LANCZOS)
+        img4=img4.resize((button_size,button_size), Image.LANCZOS)
         self.photoimg4=ImageTk.PhotoImage(img4)
 
         b1=Button(bg_img, image=self.photoimg4, cursor="hand2")
-        b1.place(x=200, y=100, width=220, height=220)
+        b1.place(x=space_button, y=space_button_y, width=button_size, height=button_size)
 
         b1_1=Button(bg_img,text="Student Details", cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
-        b1_1.place(x=200, y=300, width=220, height=40)
+        b1_1.place(x=space_button, y=space_button_y+button_size-click_size, width=button_size, height=40)
 
 
         #Detect face button 
         img5 = Image.open(r"assets/collectedImages/face_detector.jpg")
-        img5=img5.resize((220,220), Image.LANCZOS)
+        img5=img5.resize((button_size,button_size), Image.LANCZOS)
         self.photoimg5=ImageTk.PhotoImage(img5)
 
         b1=Button(bg_img, image=self.photoimg5, cursor="hand2")
-        b1.place(x=500, y=100, width=220, height=220)
+        b1.place(x=2*space_button+button_size, y=space_button_y, width=220, height=220)
 
         b1_1=Button(bg_img,text="Face Detector", cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
-        b1_1.place(x=500, y=300, width=220, height=40)  
+        b1_1.place(x=2*space_button+button_size, y=space_button_y+button_size-click_size, width=button_size, height=40)  
 
 
 
         #Attendace face button 
         img6 = Image.open(r"assets/collectedImages/attendace_face.jpg")
-        img6=img6.resize((220,220), Image.LANCZOS)
+        img6=img6.resize((button_size,button_size), Image.LANCZOS)
         self.photoimg6=ImageTk.PhotoImage(img6)
 
         b1=Button(bg_img, image=self.photoimg6, cursor="hand2")
-        b1.place(x=800, y=100, width=220, height=220)
+        b1.place(x=3*space_button+2*button_size, y=space_button_y, width=button_size, height=220)
 
-        b1_1=Button(bg_img,text="Attendace", cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
-        b1_1.place(x=800, y=300, width=220, height=40)  
+        b1_1=Button(bg_img,text="Attendance", cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=3*space_button+2*button_size, y=space_button_y+button_size-click_size, width=220, height=40)  
 
 
         #Help face button 
@@ -95,10 +95,10 @@ class Student_Attendance_Sys:
         self.photoimg7=ImageTk.PhotoImage(img7)
 
         b1=Button(bg_img, image=self.photoimg7, cursor="hand2")
-        b1.place(x=1100, y=100, width=220, height=220)
+        b1.place(x=screen_width-space_button-button_size, y=space_button_y, width=220, height=220)
 
         b1_1=Button(bg_img,text="Help Desk", cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
-        b1_1.place(x=1100, y=300, width=220, height=40)  
+        b1_1.place(x=screen_width-space_button-button_size, y=space_button_y+button_size-click_size, width=220, height=40)  
 
 
         #Train face button 
@@ -107,9 +107,9 @@ class Student_Attendance_Sys:
         self.photoimg8=ImageTk.PhotoImage(img8)
 
         b1=Button(bg_img, image=self.photoimg8, cursor="hand2")
-        b1.place(x=200, y=380, width=220, height=220)
+        b1.place(x=space_button, y=2*space_button_y+button_size, width=220, height=220)
         b1_1=Button(bg_img,text="Train Data", cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
-        b1_1.place(x=200, y=580, width=220, height=40)  
+        b1_1.place(x=space_button, y=2*space_button_y+2*button_size-click_size, width=220, height=40)  
 
 
         #Photos face button 
@@ -118,9 +118,9 @@ class Student_Attendance_Sys:
         self.photoimg9=ImageTk.PhotoImage(img9)
 
         b1=Button(bg_img, image=self.photoimg9, cursor="hand2")
-        b1.place(x=500, y=380, width=220, height=220)
+        b1.place(x=2*space_button+button_size, y=2*space_button_y+button_size, width=220, height=220)
         b1_1=Button(bg_img,text="Photos", cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
-        b1_1.place(x=500, y=580, width=220, height=40)  
+        b1_1.place(x=2*space_button+button_size, y=2*space_button_y+2*button_size-click_size, width=220, height=40)  
 
 
         #Developer face button 
@@ -129,9 +129,9 @@ class Student_Attendance_Sys:
         self.photoimg10=ImageTk.PhotoImage(img10)
 
         b1=Button(bg_img, image=self.photoimg10, cursor="hand2")
-        b1.place(x=800, y=380, width=220, height=220)
+        b1.place(x=3*space_button+2*button_size, y=2*space_button_y+button_size, width=220, height=220)
         b1_1=Button(bg_img,text="Developer", cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
-        b1_1.place(x=800, y=580, width=220, height=40)  
+        b1_1.place(x=3*space_button+2*button_size, y=2*space_button_y+2*button_size-click_size, width=220, height=40)  
 
 
 
@@ -141,17 +141,22 @@ class Student_Attendance_Sys:
         self.photoimg11=ImageTk.PhotoImage(img11)
 
         b1=Button(bg_img, image=self.photoimg11, cursor="hand2")
-        b1.place(x=1100, y=380, width=220, height=220)
+        b1.place(x=screen_width-space_button-button_size, y=2*space_button_y+button_size, width=220, height=220)
         b1_1=Button(bg_img,text="Exit", cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
-        b1_1.place(x=1100, y=580, width=220, height=40)  
+        b1_1.place(x=screen_width-space_button-button_size, y=2*space_button_y+2*button_size-click_size, width=220, height=40)  
 
 
 
 
 if __name__ == "__main__":
     root = Tk() 
+    button_size = 220
+    click_size = button_size/6
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
+    button_screen_height = (2*root.winfo_screenheight())/3
+    space_button_y = button_screen_height / 8
+    space_button = root.winfo_screenwidth()/14
     window_width = int(screen_width)
     window_height = int(screen_height)
     root.state("zoomed") #Auto fullscreen
